391d6a974cb6b92cee81a7aac6b21bdfb0173b76
GuilhermeDexheimer <guigui_dex@hotmail.com>
2025-04-27 20:17:42 -0300

    Conversor de Temperatura
    Converte Celsius para Fahrenheint e vice versa

---
+++ ## Conversor de Temperatura.py
@@ -0,0 +1,11 @@
+## Conversor de Temperatura
+
+# Celsius para Fahrenheint
+
+valor_celsius = int(input('Informe a temperatura em Celsius: ' )) 
+print ((9/5) * valor_celsius + 32)
+
+# Fahrenheint para Celsius
+
+valor_fahrenheit = int(input('Informe a temperatura em Fahrenheit: ' )) 
+print ((valor_fahrenheit - 32) * 5 / 9)
