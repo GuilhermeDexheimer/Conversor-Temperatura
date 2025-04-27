d546cc23f35473493e0bdb70e921eb5a7f9523f8
GuilhermeDexheimer <guigui_dex@hotmail.com>
2025-04-27 20:38:46 -0300

    Melhoria

--- ## Conversor de Temperatura.py
+++ ## Conversor de Temperatura.py
@@ -2,10 +2,10 @@
 
 # Celsius para Fahrenheint
 
-valor_celsius = int(input('Informe a temperatura em Celsius: ' )) 
-print ((9/5) * valor_celsius + 32)
+valor_celsius = float(input('Informe a temperatura em Celsius: ' )) 
+print('Temperatura em graus Fahrenheint: ' (9/5) * valor_celsius + 32)
 
 # Fahrenheint para Celsius
 
-valor_fahrenheit = int(input('Informe a temperatura em Fahrenheit: ' )) 
-print ((valor_fahrenheit - 32) * 5 / 9)
+valor_fahrenheit = float(input('Informe a temperatura em Fahrenheit: ' )) 
+print ('Temperatura em graus Celsius: '(valor_fahrenheit - 32) * 5 / 9)
